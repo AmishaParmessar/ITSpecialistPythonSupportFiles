b108ecabcea2b363625524789f429b1ffe7fc9fd
AmishaParmessar <144694680+AmishaParmessar@users.noreply.github.com>
2024-05-23 09:34:30 -0400

    Update 126-list_operations.py

--- Domain 1/Student/126-list_operations.py
+++ Domain 1/Student/126-list_operations.py
@@ -1,4 +1,7 @@
 capitals=["Montgomery","Juneau","Phoenix"]
+capitals.append("Sacramento")
+capitals.insert(3, "LittleRock")
+capitals.remove("Juneau") #Montgomery, Phoenix, LittleRock, Sacramento 
 
 
 print(capitals)
