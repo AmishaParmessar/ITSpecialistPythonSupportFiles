58cbeb62aa2cb0e0e5411afb9a4e563538eba414
AmishaParmessar <144694680+AmishaParmessar@users.noreply.github.com>
2024-05-28 09:22:37 -0400

    Update 221-while.py

--- Domain 2/Student/221-while.py
+++ Domain 2/Student/221-while.py
@@ -1,7 +1,8 @@
 score = 0
-score_increase = 10
+score_increase = 0
 
 while score <= 100:
    print("You have", score, "points.") 
    score += score_increase
-
+   
+print("You have reached 100 points. Congratulations!")
